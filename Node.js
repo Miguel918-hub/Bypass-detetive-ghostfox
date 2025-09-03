c6f7f953f3409e063dd0789a8800a4856f12de5a
Miguel918-hub <migueldiscord970@gmail.com>
2025-09-03 01:05:28 -0300

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,13 @@
+/bypass-site
+│
+├─ public/
+│   ├─ index.html
+│   ├─ style.css
+│   ├─ script.js
+│   └─ images/
+│       ├─ logo.png
+│       ├─ hero-image.png
+│       └─ outras imagens...
+│
+├─ api/
+│   └─ bypass.js      ← função serverless que processa os links
